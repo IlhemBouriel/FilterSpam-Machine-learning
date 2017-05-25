d35781843ee681eac0d00be51d38bc2d65cceb2c
sdk54 <ilhem.bouriel94@gmail.com>
2017-05-25 20:10:37 +0100

    Filter spam detection

---
+++ FilterSpamML/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NaiveBayes;
+
+
+namespace FilterSpamML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NaiveBayes.NaiveBayes naiveBayes = new NaiveBayes.NaiveBayes();
+            // spam mail
+            var result = naiveBayes.CheckEmail("Buy Cheap cialis Online");
+            Assert.AreEqual(true, result);
+            Console.WriteLine(result.ToString());
+            // not spam mail
+            result = naiveBayes.CheckEmail("Thanks in advance for your support");
+            Assert.AreEqual(false, result);
+            Console.WriteLine(result.ToString());
+            Console.ReadKey();
+        }
+    }
+}
